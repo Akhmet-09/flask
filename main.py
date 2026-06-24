fea978d3dde13243941f8f288a13b869c9902b6e
Akhmet-09 <muratbaevakhmet280@gmail.com>
2026-06-23 22:46:16 -1100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define the route for the home page
+@app.route('/')
+def home():
+    return '<h1>Welcome to my simple Flask website!</h1><p>It works perfectly.</p>'
+
+# Run the application if this file is executed directly
+if __name__ == '__main__':
+    app.run(debug=True)
